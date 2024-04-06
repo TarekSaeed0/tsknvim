cf75a6fbae72801a9fc5d1453f60ddcbd8690b4c
TarekSaeed0 <naminode2020@gmail.com>
2024-04-06 12:58:47 +0200

    improved neovim start up look

--- lua/tsknvim/plugins/alpha.lua
+++ lua/tsknvim/plugins/alpha.lua
@@ -114,9 +114,10 @@ return {
 					},
 				},
 			}
+
 			math.randomseed(os.time())
 			local header = headers[math.random(#headers)]
-			if #header.big + 2 * #theme.section.buttons.val + 1 <= vim.opt.lines:get() and vim.fn.strchars(header.big[1]) < vim.opt.columns:get() then
+			if #header.big + 2 * #theme.section.buttons.val + 1 <= vim.opt.lines:get() and vim.fn.strdisplaywidth(header.big[1]) < vim.opt.columns:get() then
 				theme.section.header.val = header.big
 			else
 				theme.section.header.val = header.small
@@ -125,45 +126,56 @@ return {
 
 			local buttons = {
 				{
-					shortcut = "n" ,
-					icon = "",
+					shortcut = "n",
+					icon = "",
 					text = "new file",
-					keymap = "<cmd>ene<cr>",
+					command = "<cmd>ene<cr>",
 				},
 				{
-					shortcut = "b" ,
+					shortcut = "b",
 					icon = "",
 					text = "file browser",
-					keymap = "<cmd>lua require(\"telescope\").extensions.file_browser.file_browser({ cwd = vim.fn.expand(\"%:p:h\") })<cr>",
+					command = "<cmd>lua require(\"telescope\").extensions.file_browser.file_browser({ cwd = vim.fn.expand(\"%:p:h\") })<cr>",
 				},
 				{
-					shortcut = "h" ,
-					icon = "",
+					shortcut = "h",
+					icon = "",
 					text = "file history",
 					command = "<cmd>lua require(\"telescope.builtin\").oldfiles()<cr>",
 				},
 				{
-					shortcut = "s" ,
-					icon = "󱎸",
+					shortcut = "s",
+					icon = "",
 					text = "search",
 					command = "<cmd>lua require(\"telescope.builtin\").grep_string({ cwd = vim.fn.expand(\"%:p:h\") })<cr>",
 				},
 				{
-					shortcut = "q" ,
+					shortcut = "q",
 					icon = "󰩈",
 					text = "quit",
 					command = "<cmd>q<cr>",
 				},
 			}
 
-			--  
+			local maximum_icon_length = 0
+			local maximum_text_length = 0
+			for _, button in ipairs(buttons) do
+				maximum_icon_length = math.max(maximum_icon_length, vim.fn.strdisplaywidth(button.icon))
+				maximum_text_length = math.max(maximum_text_length, vim.fn.strdisplaywidth(button.text))
+			end
+
+			for _, button in ipairs(buttons) do
+				button.icon = button.icon..(" "):rep(maximum_icon_length - vim.fn.strdisplaywidth(button.icon))
+				button.text = button.text..(" "):rep(maximum_text_length - vim.fn.strdisplaywidth(button.text))
+			end
+
+			local colors = require("catppuccin.palettes").get_palette()
+
 			theme.section.buttons.val = {}
-			local icon_length = 0
-			local text_length = 0
 			for _, button in ipairs(buttons) do
 				table.insert(theme.section.buttons.val, {
 					type = "button",
-					val = button.text,
+					val = " "..button.icon.."  "..button.text.." ",
 					on_press = function()
 						vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(button.command, true, false, true), "t", false)
 					end,
@@ -175,15 +187,14 @@ return {
 							{ noremap = true, silent = true, nowait = true },
 						},
 						hl = {
-							{ "NormalNC", 0, -3 },
-							{ "AlphaShortcut", 0, -18 },
-							{ "FloatTitle", 3, -23 },
+							{ "AlphaButtonSegment1", 0, (""):len() },
+							{ "AlphaButtonSegment2", (""):len(), (" "..button.icon.." "):len() },
+							{ "AlphaButtonSegment1", (" "..button.icon.." "):len(), (" "..button.icon.." "):len() },
+							{ "AlphaButtonSegment3", (" "..button.icon.." "):len(), (" "..button.icon.."  "..button.text.." "):len() },
+							{ "AlphaButtonSegment4", (" "..button.icon.."  "..button.text.." "):len(), (" "..button.icon.."  "..button.text.." "):len() },
 						},
 						position = "center",
-						shortcut = button.shortcut,
-						align_shortcut = "right",
-						hl_shortcut = "AlphaShortcut",
-						cursor = 3,
+						cursor = -2,
 						width = math.min(50, vim.fn.strchars(theme.section.header.val[1])),
 					}
 				})
