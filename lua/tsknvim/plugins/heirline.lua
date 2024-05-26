67372244bf55942c7106f9d265d026e22e3b6251
TarekSaeed0 <naminode2020@gmail.com>
2024-05-26 20:38:55 +0300

    fix folds not displaying correctly in status column when not sorted in ufo.nvim, fix and improve mode in status line, and change lsp hover keymap to K

--- lua/tsknvim/plugins/heirline.lua
+++ lua/tsknvim/plugins/heirline.lua
@@ -9,38 +9,30 @@ return {
 
 			local mode = {
 				init = function(self)
-					self.name = self.names[vim.api.nvim_get_mode().mode]
+					local mode = vim.api.nvim_get_mode().mode
+					self.name = vim.iter(mode:gmatch("."))
+						:enumerate()
+						:map(function(i)
+							return mode:sub(1, -i)
+						end)
+						:map(function(s)
+							return self.names[s]
+						end)
+						:next() or mode
 				end,
 				static = {
 					names = {
 						n = "NORMAL",
 						no = "O-PENDING",
-						nov = "O-PENDING",
-						noV = "O-PENDING",
-						["no\22"] = "O-PENDING",
-						niI = "NORMAL",
-						niR = "NORMAL",
-						niV = "NORMAL",
-						nt = "NORMAL",
-						ntT = "NORMAL",
 						v = "VISUAL",
-						vs = "VISUAL",
 						V = "V-LINE",
-						Vs = "V-LINE",
 						["\22"] = "V-BLOCK",
-						["\22s"] = "V-BLOCK",
 						s = "SELECT",
 						S = "S-LINE",
 						["\19"] = "S-BLOCK",
 						i = "INSERT",
-						ic = "INSERT",
-						ix = "INSERT",
 						R = "REPLACE",
-						Rc = "REPLACE",
-						Rx = "REPLACE",
 						Rv = "V-REPLACE",
-						Rvc = "V-REPLACE",
-						Rvx = "V-REPLACE",
 						c = "COMMAND",
 						cv = "EX",
 						r = "REPLACE",
@@ -294,211 +286,221 @@ return {
 			}
 			table.insert(statusline, cursor)
 
+			local tabline = { hl = "TabLine" }
+
 			local buffers = {
-				static = {
-					buffer = {
-						{
-							provider = function(self)
-								return self.is_active and "" or " "
-							end,
-							hl = { fg = "base", bg = "mantle" },
-						},
-						{
-							init = function(self)
-								local name = vim.api.nvim_buf_get_name(self.buffer)
-								local stat = vim.loop.fs_stat(name)
-								if stat and stat.type == "directory" then
-									self.icon, self.color = "", require("nvim-web-devicons").get_default_icon().color
-								else
-									self.icon, self.color = require("nvim-web-devicons").get_icon_color(
-										vim.fn.fnamemodify(name, ":t"),
-										vim.fn.fnamemodify(name, ":e")
-									)
-								end
-							end,
+				{
+					static = {
+						buffer = {
 							{
 								provider = function(self)
-									return " " .. self.icon
+									return self.is_active and "" or " "
 								end,
-								hl = function(self)
-									return { fg = self.color, bold = false }
-								end,
+								hl = { fg = "base", bg = "mantle" },
 							},
-						},
-						{
-							init = function(self)
-								local maximum_length = math.floor(vim.opt.columns:get() / math.max(#self.buffers, 4))
-									- 8
+							{
+								init = function(self)
+									local name = vim.api.nvim_buf_get_name(self.buffer)
+									local stat = vim.loop.fs_stat(name)
+									if stat and stat.type == "directory" then
+										self.icon, self.color =
+											"", require("nvim-web-devicons").get_default_icon().color
+									else
+										self.icon, self.color = require("nvim-web-devicons").get_icon_color(
+											vim.fn.fnamemodify(name, ":t"),
+											vim.fn.fnamemodify(name, ":e")
+										)
+									end
+								end,
+								{
+									provider = function(self)
+										return " " .. self.icon
+									end,
+									hl = function(self)
+										return { fg = self.color, bold = false }
+									end,
+								},
+							},
+							{
+								init = function(self)
+									local maximum_length = math.floor(
+										vim.opt.columns:get() / math.max(#self.buffers, 4)
+									) - 8
 
-								local separator = package.config:sub(1, 1)
-								local components = vim.split(
-									vim.fn.fnamemodify(vim.api.nvim_buf_get_name(self.buffer), ":~:."),
-									separator
-								)
+									local separator = package.config:sub(1, 1)
+									local components = vim.split(
+										vim.fn.fnamemodify(vim.api.nvim_buf_get_name(self.buffer), ":~:."),
+										separator
+									)
 
-								local parent = ""
-								local name = components[#components]
+									local parent = ""
+									local name = components[#components]
 
-								local length = #"…" + #separator + #name
-								for i = #components - 1, 1, -1 do
-									local component = components[i]
+									local length = #"…" + #separator + #name
+									for i = #components - 1, 1, -1 do
+										local component = components[i]
 
-									length = length + #component + #separator
-									if length > maximum_length then
-										parent = "…" .. separator .. parent
-										break
+										length = length + #component + #separator
+										if length > maximum_length then
+											parent = "…" .. separator .. parent
+											break
+										end
+
+										parent = component .. separator .. parent
+									end
+									if name == "" then
+										parent, name = name, parent
 									end
 
-									parent = component .. separator .. parent
-								end
-								if name == "" then
-									parent, name = name, parent
-								end
-
-								self.parent = parent
-								self.name = name
-							end,
-							{
-								provider = function(self)
-									return " " .. self.parent
+									self.parent = parent
+									self.name = name
 								end,
-								hl = { fg = "overlay0", bold = false },
+								{
+									provider = function(self)
+										return " " .. self.parent
+									end,
+									hl = { fg = "overlay0", bold = false },
+								},
+								{
+									provider = function(self)
+										return self.name .. " "
+									end,
+								},
+							},
+							{
+								provider = "● ",
+								hl = { fg = "green", bold = false },
+								condition = function(self)
+									return vim.api.nvim_get_option_value("modified", { buf = self.buffer })
+								end,
+							},
+							{
+								provider = " ",
+								hl = { fg = "yellow", bold = false },
+								condition = function(self)
+									return not vim.api.nvim_get_option_value("modifiable", { buf = self.buffer })
+										or vim.api.nvim_get_option_value("readonly", { buf = self.buffer })
+								end,
+							},
+							{
+								{
+									provider = "",
+									on_click = {
+										callback = function(_, buffer)
+											vim.schedule(function()
+												if vim.api.nvim_buf_is_valid(buffer) then
+													vim.api.nvim_buf_delete(buffer, { force = false })
+												end
+												vim.cmd.redrawtabline()
+											end)
+										end,
+										minwid = function(self)
+											return self.buffer
+										end,
+										name = "heirline_buffer_close_callback",
+									},
+								},
+								{ provider = " " },
+								condition = function(self)
+									return not vim.api.nvim_get_option_value("modified", { buf = self.buffer })
+								end,
 							},
 							{
 								provider = function(self)
-									return self.name .. " "
+									return self.is_active and "" or " "
 								end,
+								hl = { fg = "base", bg = "mantle" },
 							},
-						},
-						{
-							provider = "● ",
-							hl = { fg = "green", bold = false },
-							condition = function(self)
-								return vim.api.nvim_get_option_value("modified", { buf = self.buffer })
+							hl = function(self)
+								return self.is_active and "TabLineSel" or "TabLine"
 							end,
-						},
-						{
-							provider = " ",
-							hl = { fg = "yellow", bold = false },
-							condition = function(self)
-								return not vim.api.nvim_get_option_value("modifiable", { buf = self.buffer })
-									or vim.api.nvim_get_option_value("readonly", { buf = self.buffer })
-							end,
-						},
-						{
-							{
-								provider = "",
-								on_click = {
-									callback = function(_, buffer)
+							on_click = {
+								callback = function(_, buffer, _, button)
+									if button == "l" then
+										vim.api.nvim_win_set_buf(0, buffer)
+									elseif button == "m" then
 										vim.schedule(function()
 											if vim.api.nvim_buf_is_valid(buffer) then
 												vim.api.nvim_buf_delete(buffer, { force = false })
 											end
-											vim.cmd.redrawtabline()
 										end)
-									end,
-									minwid = function(self)
-										return self.buffer
-									end,
-									name = "heirline_buffer_close_callback",
-								},
+									end
+								end,
+								minwid = function(self)
+									return self.buffer
+								end,
+								name = "heirline_buffer_callback",
 							},
-							{ provider = " " },
-							condition = function(self)
-								return not vim.api.nvim_get_option_value("modified", { buf = self.buffer })
-							end,
-						},
-						{
-							provider = function(self)
-								return self.is_active and "" or " "
-							end,
-							hl = { fg = "base", bg = "mantle" },
-						},
-						hl = function(self)
-							return self.is_active and "TabLineSel" or "TabLine"
-						end,
-						on_click = {
-							callback = function(_, buffer, _, button)
-								if button == "m" then
-									vim.schedule(function()
-										if vim.api.nvim_buf_is_valid(buffer) then
-											vim.api.nvim_buf_delete(buffer, { force = false })
-										end
-									end)
-								else
-									vim.api.nvim_win_set_buf(0, buffer)
-								end
-							end,
-							minwid = function(self)
-								return self.buffer
-							end,
-							name = "heirline_buffer_callback",
 						},
 					},
+					init = function(self)
+						self.buffers = vim.tbl_filter(function(buffer)
+							return vim.api.nvim_buf_is_valid(buffer)
+								and vim.api.nvim_get_option_value("buflisted", { buf = buffer })
+						end, vim.api.nvim_list_bufs())
+
+						for index, buffer in ipairs(self.buffers) do
+							local child = self[index]
+							if not (child and child.buffer == buffer) then
+								self[index] = self:new(self.buffer, index)
+								child = self[index]
+								child.buffer = buffer
+							end
+
+							if buffer == tonumber(vim.g.actual_curbuf) and utils.in_focus then
+								child.is_active = true
+							else
+								child.is_active = false
+							end
+						end
+						if #self > #self.buffers then
+							for index = #self.buffers + 1, #self do
+								self[index] = nil
+							end
+						end
+					end,
 				},
-				init = function(self)
-					self.buffers = vim.tbl_filter(function(buffer)
-						return vim.api.nvim_buf_is_valid(buffer)
-							and vim.api.nvim_get_option_value("buflisted", { buf = buffer })
-					end, vim.api.nvim_list_bufs())
-
-					for index, buffer in ipairs(self.buffers) do
-						local child = self[index]
-						if not (child and child.buffer == buffer) then
-							self[index] = self:new(self.buffer, index)
-							child = self[index]
-							child.buffer = buffer
-						end
-
-						if buffer == tonumber(vim.g.actual_curbuf) and utils.in_focus then
-							child.is_active = true
-						else
-							child.is_active = false
-						end
-					end
-					if #self > #self.buffers then
-						for index = #self.buffers + 1, #self do
-							self[index] = nil
-						end
-					end
-				end,
-			}
-
-			local tabline = {
-				buffers,
 				{
 					provider = " ",
 					on_click = {
-						callback = function()
-							vim.cmd.enew()
+						callback = function(_, _, _, button)
+							if button == "l" then
+								vim.cmd.enew()
+							end
 						end,
 						name = "heirline_new_callback",
 					},
 					hl = { fg = "green", bg = "mantle", bold = true },
 				},
-				{ provider = "%=" },
-				{
-					{
-						provider = "╲",
-						hl = { fg = "red", bg = "mantle" },
-					},
-					{
-						provider = "  ",
-						hl = { fg = "mantle", bg = "red" },
-						on_click = {
-							callback = function()
-								vim.cmd.quit()
-							end,
-							name = "heirline_quit_callback",
-						},
-					},
-					{
-						provider = "",
-						hl = { fg = "red" },
-					},
-					hl = { bold = true },
-				},
 			}
+			table.insert(tabline, buffers)
+
+			table.insert(tabline, { provider = "%=" })
+
+			local quit = {
+				{
+					provider = "╲",
+					hl = { fg = "red" },
+				},
+				{
+					provider = "  ",
+					hl = { fg = "mantle", bg = "red" },
+					on_click = {
+						callback = function(_, _, _, button)
+							if button == "l" then
+								vim.cmd.quit()
+							end
+						end,
+						name = "heirline_quit_callback",
+					},
+				},
+				{
+					provider = "",
+					hl = { fg = "red" },
+				},
+				hl = { bold = true },
+			}
+			table.insert(tabline, quit)
 
 			local sign = {
 				provider = "%s",
@@ -511,20 +513,11 @@ return {
 			local fold = {
 				static = {
 					is_fold_start = function(buffer, line)
-						local folds = (require("ufo.fold").get(buffer) or { foldRanges = {} }).foldRanges
-						local low = 1
-						local high = #folds
-						while low <= high do
-							local middle = math.floor((low + high) / 2)
-							if folds[middle].startLine + 1 == line then
-								return true
-							elseif folds[middle].startLine + 1 > line then
-								high = middle - 1
-							else
-								low = middle + 1
-							end
-						end
-						return false
+						local fold_buffer = require("ufo.fold").get(buffer)
+						return fold_buffer
+							and vim.iter(fold_buffer.foldRanges):any(function(range)
+								return range.startLine + 1 == line
+							end)
 					end,
 				},
 				provider = function(self)
