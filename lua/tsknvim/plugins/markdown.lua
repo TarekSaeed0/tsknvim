7cf0b84ba46847a53a8ee1479e93f4e7cec6ffe6
TarekSaeed0 <naminode2020@gmail.com>
2024-08-10 13:43:39 +0300

    replaced markdown.nvim with markview.nvim

--- lua/tsknvim/plugins/markdown.lua
+++
@@ -1,9 +0,0 @@
-return {
-	{
-		"MeanderingProgrammer/markdown.nvim",
-		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-		opts = {},
-		main = "render-markdown",
-		ft = "markdown",
-	},
-}
