c8e978ba2c7327af44a636000f1c06cf87552a39
TarekSaeed0 <naminode2020@gmail.com>
2024-09-13 13:56:56 +0300

    refactor(plugins): improve lsp_lines.nvim lazy loading
    lsp_lines.nvim now loads on the first diagnostic message instead of on
    lsp client attaches, this also fixes lsp_lines.nvim not loading when a linter
    shows a diagnostic message but there isn't a lsp client attached

--- lua/tsknvim/plugins/lsp_lines.lua
+++ lua/tsknvim/plugins/lsp_lines.lua
@@ -2,12 +2,22 @@
 return {
 	{
 		"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+		lazy = true,
+		init = function()
+			vim.api.nvim_create_autocmd("DiagnosticChanged", {
+				callback = function(args)
+					if #args.data.diagnostics ~= 0 then
+						require("lazy.core.loader").load({ "lsp_lines.nvim" }, { event = "DiagnosticChanged" })
+						return true
+					end
+				end,
+			})
+		end,
 		config = function()
 			require("lsp_lines").setup()
 
 			vim.diagnostic.config({ virtual_lines = { highlight_whole_line = false } })
 			vim.diagnostic.config({ virtual_lines = false }, vim.api.nvim_create_namespace("lazy"))
 		end,
-		event = "LspAttach",
 	},
 }
