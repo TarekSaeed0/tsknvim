c751e46050917370e58ca80e4c6a13350ffc57c5
TarekSaeed0 <naminode2020@gmail.com>
2024-10-12 06:43:30 +0300

    refactor(lsp): move on_attach to tsknvim.utils
    - remove duplicate definitions of on_attach and define it only in tsknvim.utils

--- lua/tsknvim/utils.lua
+++ lua/tsknvim/utils.lua
@@ -55,4 +55,60 @@ M.select_file = function(callback)
 	})
 end
 
+M.lsp = {}
+M.lsp.on_attach = function(_, buffer)
+	vim.api.nvim_set_option_value("omnifunc", "v:lua.vim.lsp.omnifunc", { buf = buffer })
+
+	vim.keymap.set({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<cr>", { buffer = buffer })
+
+	vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<cr>", { buffer = buffer })
+	vim.keymap.set("n", "<leader>rnp", "<cmd>Lspsaga rename ++project<cr>", { buffer = buffer })
+
+	vim.keymap.set("n", "<leader>pd", "<cmd>Lspsaga peek_definition<cr>", { buffer = buffer })
+	vim.keymap.set("n", "<leader>gd", "<cmd>Lspsaga goto_definition<cr>", { buffer = buffer })
+
+	vim.keymap.set("n", "<leader>pt", "<cmd>Lspsaga peek_type_definition<cr>", { buffer = buffer })
+	vim.keymap.set("n", "<leader>gt", "<cmd>Lspsaga goto_type_definition<cr>", { buffer = buffer })
+
+	vim.keymap.set("n", "<leader>ld", "<cmd>Lspsaga show_line_diagnostics<cr>", { buffer = buffer })
+	vim.keymap.set("n", "<leader>bd", "<cmd>Lspsaga show_buf_diagnostics<cr>", { buffer = buffer })
+	vim.keymap.set("n", "<leader>wd", "<cmd>Lspsaga show_workspace_diagnostics<cr>", { buffer = buffer })
+	vim.keymap.set("n", "<leader>cd", "<cmd>Lspsaga show_cursor_diagnostics<cr>", { buffer = buffer })
+
+	vim.keymap.set(
+		"n",
+		"[d",
+		"<cmd>Lspsaga diagnostic_jump_prev<cr>",
+		{ buffer = buffer, desc = "Previous diagnostic" }
+	)
+	vim.keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<cr>", { buffer = buffer, desc = "Next diagnostic" })
+
+	for key, severity in pairs({ e = "ERROR", w = "WARN", i = "INFO", h = "HINT" }) do
+		vim.keymap.set("n", "[" .. key, function()
+			require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity[severity] })
+		end, { buffer = buffer, desc = "Previous " .. severity:lower() .. " diagnostic" })
+		vim.keymap.set("n", "]" .. key, function()
+			require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity[severity] })
+		end, { buffer = buffer, desc = "Next " .. severity:lower() .. " diagnostic" })
+	end
+
+	vim.keymap.set("n", "<leader>o", "<cmd>Lspsaga outline<cr>", { buffer = buffer })
+
+	if require("tsknvim.utils").is_loaded("nvim-ufo") then
+		vim.keymap.set("n", "K", function()
+			if not require("ufo").peekFoldedLinesUnderCursor(false, false) then
+				vim.cmd.Lspsaga("hover_doc")
+			end
+		end, { buffer = buffer })
+	else
+		vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<cr>", { buffer = buffer })
+	end
+	vim.keymap.set("n", "K<space>", "<cmd>Lspsaga hover_doc ++keep<cr>", { buffer = buffer })
+
+	vim.keymap.set("n", "<leader>ih", function()
+		vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = buffer }), { bufnr = buffer })
+	end, { buffer = buffer, desc = "Toggle inlay hints" })
+	vim.lsp.inlay_hint.enable(true, { bufnr = buffer })
+end
+
 return M
