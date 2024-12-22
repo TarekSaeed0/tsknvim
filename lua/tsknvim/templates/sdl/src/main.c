d89bbd73aaed5c758bd1e18e65a77e007813d8cf
TarekSaeed0 <naminode2020@gmail.com>
2024-12-22 19:37:20 +0200

    .

--- lua/tsknvim/templates/sdl/src/main.c
+++ lua/tsknvim/templates/sdl/src/main.c
@@ -18,9 +18,12 @@ enum {
 struct application {
 	SDL_Renderer *renderer;
 	SDL_Window *window;
-	SDL_Texture *text_texture;
-	SDL_Rect text_rect;
-	float delta_time;
+	SDL_Texture *background_texture;
+	struct {
+		SDL_Texture *texture;
+		int width;
+		int height;
+	} text;
 };
 int application_create(struct application *application) {
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER)) {
@@ -32,13 +35,23 @@ int application_create(struct application *application) {
 		goto no_cleanup;
 	}
 
+	int flags = IMG_INIT_PNG;
+	if (!((unsigned)IMG_Init(flags) & (unsigned)flags)) {
+		SDL_LogError(
+			SDL_LOG_CATEGORY_VIDEO,
+			"failed to initalize SDL_image: IMG_Error: %s\n",
+			IMG_GetError()
+		);
+		goto sdl_cleanup;
+	}
+
 	if (TTF_Init() == -1) {
 		SDL_LogError(
 			SDL_LOG_CATEGORY_VIDEO,
 			"failed to initalize SDL_ttf: TTF_Error: %s\n",
 			TTF_GetError()
 		);
-		goto sdl_cleanup;
+		goto img_cleanup;
 	}
 
 	application->window = SDL_CreateWindow(
@@ -47,7 +60,7 @@ int application_create(struct application *application) {
 		SDL_WINDOWPOS_UNDEFINED,
 		APPLICATION_DEFAULT_WINDOW_WIDTH,
 		APPLICATION_DEFAULT_WINDOW_HEIGHT,
-		0
+		SDL_WINDOW_RESIZABLE
 	);
 	if (!application->window) {
 		SDL_LogError(
@@ -70,6 +83,19 @@ int application_create(struct application *application) {
 
 	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1");
 
+	const char *background_texture_path = "data/speech_bubble.png";
+	application->background_texture =
+		IMG_LoadTexture(application->renderer, background_texture_path);
+	if (!application->background_texture) {
+		SDL_LogError(
+			SDL_LOG_CATEGORY_VIDEO,
+			"failed to load image \"%s\": IMG_Error: %s\n",
+			background_texture_path,
+			IMG_GetError()
+		);
+		goto renderer_cleanup;
+	}
+
 	const char *font_path = "data/arial.ttf";
 	TTF_Font *font = TTF_OpenFont(font_path, 64);
 	if (!font) {
@@ -79,11 +105,15 @@ int application_create(struct application *application) {
 			font_path,
 			TTF_GetError()
 		);
-		goto renderer_cleanup;
+		goto texture_cleanup;
 	}
 
-	SDL_Surface *surface =
-		TTF_RenderText_Blended_Wrapped(font, "Hello, #{in_pascal_case(name):gsub("(%l)(%u)", "%1 %2")}#!", (SDL_Color){ 0, 0, 0, UINT8_MAX }, 0);
+	SDL_Surface *surface = TTF_RenderText_Blended_Wrapped(
+		font,
+		"Hello, #{in_pascal_case(name):gsub("(%l)(%u)", "%1 %2")}#!",
+		(SDL_Color){ 0, 0, 0, UINT8_MAX },
+		0
+	);
 	if (surface == NULL) {
 		SDL_LogError(
 			SDL_LOG_CATEGORY_VIDEO,
@@ -93,63 +123,39 @@ int application_create(struct application *application) {
 		goto font_cleanup;
 	}
 
-	application->text_texture = SDL_CreateTextureFromSurface(application->renderer, surface);
-	application->text_rect.x = (APPLICATION_DEFAULT_WINDOW_WIDTH - surface->w) / 2;
-	application->text_rect.y = (APPLICATION_DEFAULT_WINDOW_HEIGHT - surface->h) / 2;
-	application->text_rect.w = surface->w;
-	application->text_rect.h = surface->h;
+	application->text.texture = SDL_CreateTextureFromSurface(application->renderer, surface);
+	application->text.width = surface->w;
+	application->text.height = surface->h;
 
 	SDL_FreeSurface(surface);
 	TTF_CloseFont(font);
 
-	application->delta_time = 0;
-
 	return EXIT_SUCCESS;
 font_cleanup:
 	TTF_CloseFont(font);
+texture_cleanup:
+	SDL_DestroyTexture(application->background_texture);
 renderer_cleanup:
 	SDL_DestroyRenderer(application->renderer);
 window_cleanup:
 	SDL_DestroyWindow(application->window);
 ttf_cleanup:
 	TTF_Quit();
+img_cleanup:
+	IMG_Quit();
 sdl_cleanup:
 	SDL_Quit();
 no_cleanup:
 	return EXIT_FAILURE;
 }
 void application_destroy(struct application *application) {
-	SDL_DestroyTexture(application->text_texture);
+	SDL_DestroyTexture(application->text.texture);
+	SDL_DestroyTexture(application->background_texture);
 	SDL_DestroyRenderer(application->renderer);
 	SDL_DestroyWindow(application->window);
 	TTF_Quit();
+	IMG_Quit();
 	SDL_Quit();
-}
-void application_graphics(struct application *application) {
-	SDL_SetRenderDrawColor(application->renderer, UINT8_MAX, UINT8_MAX, UINT8_MAX, UINT8_MAX);
-	SDL_RenderClear(application->renderer);
-
-	SDL_SetRenderDrawColor(application->renderer, 0, 0, 0, 0);
-
-	SDL_RenderCopy(application->renderer, application->text_texture, NULL, &application->text_rect);
-
-	SDL_RenderPresent(application->renderer);
-}
-void application_step(struct application *application) {
-	Uint64 start_time = SDL_GetTicks64();
-
-	application_graphics(application);
-
-	Uint64 end_time = SDL_GetTicks64();
-	if (1000 / 60 > (end_time - start_time)) {
-		SDL_Delay(1000 / 60 - (Uint32)(end_time - start_time));
-		end_time = SDL_GetTicks64();
-	}
-	application->delta_time = (float)(end_time - start_time) / 1000;
-}
-void application_handle_event(struct application *application, SDL_Event *event) {
-	(void)application;
-	switch (event->type) { default:; }
 }
 void application_run(struct application *application) {
 	while (true) {
@@ -158,10 +164,49 @@ void application_run(struct application *application) {
 			if (event.type == SDL_QUIT) {
 				return;
 			}
-			application_handle_event(application, &event);
 		}
 
-		application_step(application);
+		SDL_SetRenderDrawColor(application->renderer, UINT8_MAX, UINT8_MAX, UINT8_MAX, UINT8_MAX);
+		SDL_RenderClear(application->renderer);
+
+		SDL_SetRenderDrawColor(application->renderer, 0, 0, 0, 0);
+
+		int window_width = 0;
+		int window_height = 0;
+		SDL_GetWindowSize(application->window, &window_width, &window_height);
+
+		SDL_RenderCopy(
+			application->renderer,
+			application->background_texture,
+			NULL,
+			&(struct SDL_Rect){ .w = window_width, .h = window_height }
+		);
+
+		SDL_FRect text_rect = {
+			.w = (float)application->text.width,
+			.h = (float)application->text.height,
+		};
+
+		enum {
+			TEXTURE_PADDING_X = 70,
+			TEXTURE_PADDING_Y = 20,
+		};
+		if (text_rect.w + 2 * TEXTURE_PADDING_X > (float)window_width) {
+			text_rect.w = (float)window_width - 2 * TEXTURE_PADDING_X;
+			text_rect.h *= text_rect.w / (float)application->text.width;
+		}
+
+		if (text_rect.h + 2 * TEXTURE_PADDING_Y > (float)window_height) {
+			text_rect.h = (float)window_height - 2 * TEXTURE_PADDING_Y;
+			text_rect.w *= text_rect.h / (float)application->text.height;
+		}
+
+		text_rect.x = ((float)window_width - text_rect.w) / 2;
+		text_rect.y = ((float)window_height - text_rect.h) / 2;
+
+		SDL_RenderCopyF(application->renderer, application->text.texture, NULL, &text_rect);
+
+		SDL_RenderPresent(application->renderer);
 	}
 }
 
